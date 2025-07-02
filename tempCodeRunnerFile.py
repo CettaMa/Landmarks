ab06e74329c185371145cbade0f664e1a439b294
CettaMa <CettaMaulana96@gmail.com>
2025-07-02 13:50:21 +0700

    update

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-    if 'XGBoost' in results:
-        xgboost_model = models['XGBoost']
-        xgboost_path = os.path.join("exported_models", "xgboost_model.pkl")
-        joblib.dump(xgboost_model, xgboost_path, compress=3)
-        print(f"XGBoost model exported to {xgboost_path}")
+_detect_objects
